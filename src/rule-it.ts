aca55fc4edf25b9e8b122d54ff4aedad55607f70
Thiago Silva <thiago.h.silva.98@gmail.com>
2023-11-24 13:40:59 -0300

    add logic to compare date values

--- src/rule-it.ts
+++ src/rule-it.ts
@@ -19,7 +19,8 @@ export class RuleIt {
     }
 
     const isArray = Array.isArray(a)
-    const type = isArray ? 'array' : typeofA
+    const isDate = a instanceof Date
+    const type = isArray ? 'array' : isDate ? 'date' : typeofA
     const allowedOperators = allowedMap.get(type)
     if (!((allowedOperators?.includes(operator)) ?? false)) {
       throw new Error(`Operador ${operator} não permitido para o tipo ${type}`)
@@ -27,9 +28,9 @@ export class RuleIt {
 
     switch (operator) {
       case 'eq':
-        return a === b
+        return isDate ? (a as Date).getTime() === (b as Date).getTime() : a === b
       case 'ne':
-        return a !== b
+        return isDate ? (a as Date).getTime() !== (b as Date).getTime() : a !== b
       case 'gt':
         return a > b
       case 'ge':
